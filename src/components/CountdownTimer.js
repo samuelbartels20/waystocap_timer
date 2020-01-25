8452063395a6a92cc5fafbe0850f1c474e8441ee
samuelbartels20 <bartelssamuel20@gmail.com>
2020-01-25 17:29:52 +0000

    feat: set countdown time

--- src/components/CountdownTimer.js
+++ src/components/CountdownTimer.js
@@ -1,12 +1,36 @@
-import React from 'react'; 
-
+import React, { useState }  from 'react'; 
+import TimeInput from './TimeInput';
 import './CountdownTimer.css';
 
 function CountdownTimer() { 
+  const [inputMinutes, setInputMinutes] = useState(''); 
+  const [isRunning, setIsRunning] = useState(false);
+  const [totalSeconds, setTotalSeconds] = useState(0);
+  const [secondsRemaining, setSecondsRemaining] = useState(0);
+
+  const onInputMinutesChange = (e, value) => {
+    setInputMinutes(value.value);
+  };
+
+  const onStart = () => {
+    const minutes = parseFloat(inputMinutes);
+
+    if (isNaN(minutes) || minutes < 0) return;
+
+    const totalSeconds = Math.floor(60 * minutes);
+
+    setIsRunning(true);
+    setSecondsRemaining(totalSeconds);
+    setTotalSeconds(totalSeconds);
+  };
     
   return (
     <div className="countdown-timer">
-       <p>timer</p>
+       <TimeInput
+        value={inputMinutes}
+        onChange={onInputMinutesChange}
+        onStart={onStart}
+      />
     </div>
   );
 }
