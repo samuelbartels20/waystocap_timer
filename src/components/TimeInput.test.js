9f3d643b873d705d160fdf2103230f945324cfef
samuelbartels20 <bartelssamuel20@gmail.com>
2020-01-25 17:22:16 +0000

    added test for timeinput component

--- src/components/TimeInput.test.js
+++ src/components/TimeInput.test.js
@@ -1,29 +1,7 @@
 import React from 'react';
-import { Form, Input, Button } from 'semantic-ui-react';
-import PropTypes from 'prop-types';
+import TimeInput from './TimeInput';
+import { shallow } from 'enzyme';
 
-import './TimeInput.css';
-
-function TimeInput({ value, onChange, onStart }) {
-  // TODO: Add validation error messages
-
-  return (
-    <Form className="time-input">
-      <Form.Field inline>
-        <label>Countdown:</label>
-        <Input placeholder="(Min)" onChange={onChange} value={value} />
-        <Button onClick={onStart} success>
-          Start
-        </Button>
-      </Form.Field>
-    </Form>
-  );
-}
-
-TimeInput.propTypes = {
-  value: PropTypes.string,
-  onChange: PropTypes.func,
-  onStart: PropTypes.func,
-};
-
-export default TimeInput;
+it('renders without crashing', () => {
+  shallow(<TimeInput />);
+});
