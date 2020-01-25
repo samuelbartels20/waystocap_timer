612dca78606f229bf05ff6de5f6a1cfbbbf6615c
samuelbartels20 <bartelssamuel20@gmail.com>
2020-01-25 16:57:31 +0000

    added main app component

---
+++ src/components/components/countdowntimer.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react'; 
+
+import './CountdownTimer.css';
+
+function CountdownTimer() { 
+
+  return (
+    <div className="countdown-timer"> 
+    </div>
+  );
+}
+
+export default CountdownTimer;
