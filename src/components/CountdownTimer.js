7cb51d020bee9675bbebba568733c266c32985f7
samuelbartels20 <bartelssamuel20@gmail.com>
2020-01-25 17:53:34 +0000

    countdown default

--- src/components/CountdownTimer.js
+++ src/components/CountdownTimer.js
@@ -1,14 +1,27 @@
-import React, { useState }  from 'react'; 
+import React, { useState, useEffect }  from 'react'; 
 import TimeInput from './TimeInput';
 import CountdownStartPause from './CountdownStartPause';
 import './CountdownTimer.css';
 import CountdownDisplay from './CountdownDisplay';
+import CountdownStatus from './CountdownStatus';
 
 function CountdownTimer() { 
   const [inputMinutes, setInputMinutes] = useState(''); 
   const [isRunning, setIsRunning] = useState(false);
   const [totalSeconds, setTotalSeconds] = useState(0);
   const [secondsRemaining, setSecondsRemaining] = useState(0);
+  const [speed, setSpeed] = useState(1);
+  
+  useEffect(() => {
+    if (isRunning) {
+      const interval = setInterval(() => {
+        setSecondsRemaining(s => s - 1);
+      }, 1000 / speed);
+      return () => clearInterval(interval);
+    }
+  }, [isRunning, speed]);
+
+  if (isRunning && secondsRemaining === 0) setIsRunning(false);
 
   const onInputMinutesChange = (e, value) => {
     setInputMinutes(value.value);
@@ -37,7 +50,10 @@ function CountdownTimer() {
         onChange={onInputMinutesChange}
         onStart={onStart}
       />
-      
+      <CountdownStatus
+        secondsRemaining={secondsRemaining}
+        totalSeconds={totalSeconds}
+      />
        <div className="display-row">
         <CountdownDisplay
           secondsRemaining={secondsRemaining}
